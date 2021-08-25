c55790e185d2e000b69e7a690ba93a54b74bb15c
Samuel leão <contato@vibecriativa.com.br>
2021-08-25 01:09:58 -0300

    Commit Second

---
+++ pages/_app.js
@@ -0,0 +1,23 @@
+import { ChakraProvider, extendTheme } from '@chakra-ui/react';
+const colors = {
+  brand: {
+    900: "#8615DF",
+    800: "#38085C",
+    700: "#190429",
+    500: "#ffffff",
+    400: "#F6F6F7",
+    300: "#666665"
+  },
+}
+
+const theme = extendTheme({colors})
+
+export default function MyApp({ Component, pageProps }) {
+  return (
+    <ChakraProvider theme={theme} >
+      <Component {...pageProps}/>
+    </ChakraProvider>
+  )
+}
+
+
